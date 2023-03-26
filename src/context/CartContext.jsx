b14b0bcdca69a56620b8e75391ab71b931956857
RedwanAhmedTapuu <redwan25880@gmail.com>
2023-03-26 11:00:14 +0600

    reducer

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -15,8 +15,8 @@ const getLocalCartData = () => {
 const initialState = {
   // cart: [],
   cart: getLocalCartData(),
-  // total_item: "",
-  // total_price: "",
+  total_item: "",
+  total_price: "",
   shipping_fee: 50000,
   // amount: 0,
 };
